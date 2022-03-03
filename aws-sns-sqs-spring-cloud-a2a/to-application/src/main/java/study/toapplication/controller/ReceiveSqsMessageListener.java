fbe99e14f8093ae2102f1d26706477ae171b0bc8
taehee-kim-dev <taehee.kim.dev@gmail.com>
2022-03-03 23:52:16 +0900

    fix: dependency 수정, yml로 변경, sqs 이름으로 변경

--- aws-sns-sqs-spring-cloud-a2a/to-application/src/main/java/study/toapplication/controller/ReceiveSqsMessageListener.java
+++ aws-sns-sqs-spring-cloud-a2a/to-application/src/main/java/study/toapplication/controller/ReceiveSqsMessageListener.java
@@ -1,15 +1,15 @@
 package study.toapplication.controller;
 
-import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
-import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.aws.messaging.listener.SqsMessageDeletionPolicy;
+import org.springframework.cloud.aws.messaging.listener.annotation.SqsListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class ReceiveSqsMessageListener {
 
-    @SqsListener(value = "https://sqs.ap-northeast-2.amazonaws.com/385423560848/taeheekim-sqs", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
+    @SqsListener(value = "taeheekim-sqs", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receive(String message) {
         log.info("수신한 메시지 : {}", message);
     }
