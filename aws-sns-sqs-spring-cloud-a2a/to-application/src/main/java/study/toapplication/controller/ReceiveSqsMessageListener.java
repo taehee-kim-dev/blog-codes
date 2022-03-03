6a04435e6ef203ddb8c3b74f3d149be6112f49b5
taehee-kim-dev <taehee.kim.dev@gmail.com>
2022-03-03 23:38:11 +0900

    fix: application.properties 내용 삭제, deletionPolicy 추가

--- aws-sns-sqs-spring-cloud-a2a/to-application/src/main/java/study/toapplication/controller/ReceiveSqsMessageListener.java
+++ aws-sns-sqs-spring-cloud-a2a/to-application/src/main/java/study/toapplication/controller/ReceiveSqsMessageListener.java
@@ -1,5 +1,6 @@
 package study.toapplication.controller;
 
+import io.awspring.cloud.messaging.listener.SqsMessageDeletionPolicy;
 import io.awspring.cloud.messaging.listener.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class ReceiveSqsMessageListener {
 
-    @SqsListener(value = "taeheekim-sqs")
+    @SqsListener(value = "taeheekim-sqs", deletionPolicy = SqsMessageDeletionPolicy.ON_SUCCESS)
     public void receive(String message) {
         log.info("수신한 메시지 : {}", message);
     }
