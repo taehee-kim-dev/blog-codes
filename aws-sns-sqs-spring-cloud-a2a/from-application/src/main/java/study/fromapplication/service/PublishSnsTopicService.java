f028259b6a6ec99bcc27fa340bd434f7b9b6fdb5
taehee-kim-dev <taehee.kim.dev@gmail.com>
2022-03-06 00:40:50 +0900

    Revert "fix: headers key 변경"
    This reverts commit b6bc19b3fb2bafaa69517cbc9b23682c8638f0b8.

--- aws-sns-sqs-spring-cloud-a2a/from-application/src/main/java/study/fromapplication/service/PublishSnsTopicService.java
+++ aws-sns-sqs-spring-cloud-a2a/from-application/src/main/java/study/fromapplication/service/PublishSnsTopicService.java
@@ -1,14 +1,8 @@
 package study.fromapplication.service;
 
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
-import com.amazonaws.services.sns.AmazonSNSClientBuilder;
-import com.amazonaws.services.sns.model.AmazonSNSException;
-import com.amazonaws.services.sns.model.PublishRequest;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Service;
 import study.fromapplication.dto.PublishSnsTopicRequest;
 
@@ -28,8 +22,8 @@ public class PublishSnsTopicService {
     public void publish(PublishSnsTopicRequest publishSnsTopicRequest) {
         Map<String, Object> headers = new HashMap<>();
         if (publishSnsTopicRequest.getFifo()) {
-            headers.put("message-group-id", publishSnsTopicRequest.getMessageGroupId());
-            headers.put("message-deduplication-id", publishSnsTopicRequest.getMessageDeduplicationId());
+            headers.put("messageGroupId", publishSnsTopicRequest.getMessageGroupId());
+            headers.put("deduplicationId", publishSnsTopicRequest.getMessageDeduplicationId());
         }
         notificationMessagingTemplate.convertAndSend(
                 publishSnsTopicRequest.getTopicArn(),
