1c8c1324d3c13bc8e1ced023491e7bbea3f4eff1
taehee-kim-dev <taehee.kim.dev@gmail.com>
2022-03-06 00:40:50 +0900

    Revert "modify: dependency, yaml 수정"
    This reverts commit c97bc9671191e9dadc4d7b016b217b2d6b730ead.

--- aws-sns-sqs-spring-cloud-a2a/from-application/src/main/java/study/fromapplication/service/PublishSnsTopicService.java
+++ aws-sns-sqs-spring-cloud-a2a/from-application/src/main/java/study/fromapplication/service/PublishSnsTopicService.java
@@ -1,36 +1,49 @@
 package study.fromapplication.service;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClientBuilder;
+import com.amazonaws.services.sns.model.AmazonSNSException;
+import com.amazonaws.services.sns.model.PublishRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Service;
 import study.fromapplication.dto.PublishSnsTopicRequest;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @Service
 public class PublishSnsTopicService {
 
-    private final NotificationMessagingTemplate notificationMessagingTemplate;
-
-    public PublishSnsTopicService(AmazonSNS amazonSNS) {
-        notificationMessagingTemplate = new NotificationMessagingTemplate(amazonSNS);
+    public void publish(PublishSnsTopicRequest publishSnsTopicRequest) {
+        AmazonSNS amazonSNS = getAmazonSns();
+        try {
+            PublishRequest request = getPublishRequestBy(publishSnsTopicRequest);
+            amazonSNS.publish(request);
+        } catch (AmazonSNSException e) {
+            log.error(e.getMessage());
+            System.exit(1);
+        }
+        log.info("메세지 전송됨. message = {}", publishSnsTopicRequest.getMessage());
     }
 
-    public void publish(PublishSnsTopicRequest publishSnsTopicRequest) {
-        Map<String, Object> headers = new HashMap<>();
-        if (publishSnsTopicRequest.getFifo()) {
-            headers.put("messageGroupId", publishSnsTopicRequest.getMessageGroupId());
-            headers.put("deduplicationId", publishSnsTopicRequest.getMessageDeduplicationId());
-        }
-        notificationMessagingTemplate.convertAndSend(
-                publishSnsTopicRequest.getTopicArn(),
-                publishSnsTopicRequest.getMessage(),
-                headers
-        );
+    public AmazonSNS getAmazonSns() {
+        return AmazonSNSClientBuilder
+            .standard()
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .build();
+    }
 
-        log.info("메세지 전송됨. message = {}", publishSnsTopicRequest.getMessage());
+    private PublishRequest getPublishRequestBy(PublishSnsTopicRequest publishSnsTopicRequest) {
+        log.info("publishSnsTopicRequest = {}", publishSnsTopicRequest);
+        if (publishSnsTopicRequest.getFifo()) {
+            return new PublishRequest()
+                    .withTopicArn(publishSnsTopicRequest.getTopicArn())
+                    .withMessage(publishSnsTopicRequest.getMessage())
+                    .withMessageGroupId(publishSnsTopicRequest.getMessageGroupId())
+                    .withMessageDeduplicationId(publishSnsTopicRequest.getMessageDeduplicationId());
+
+        }
+        return new PublishRequest()
+            .withTopicArn(publishSnsTopicRequest.getTopicArn())
+            .withMessage(publishSnsTopicRequest.getMessage());
     }
 }
