79dc35d8a4e4c8a4e6af5a9266fd17375ca0db1b
taehee-kim-dev <taehee.kim.dev@gmail.com>
2022-06-17 16:57:02 +0900

    eventlistener-transaction 모듈 생성

---
+++ eventlistener-transaction/src/main/java/taeheekim/eventlistenertransaction/EventlistenerTransactionApplication.java
@@ -0,0 +1,13 @@
+package taeheekim.eventlistenertransaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventlistenerTransactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventlistenerTransactionApplication.class, args);
+	}
+
+}
---
+++ eventlistener-transaction/src/test/java/taeheekim/eventlistenertransaction/EventlistenerTransactionApplicationTests.java
@@ -0,0 +1,13 @@
+package taeheekim.eventlistenertransaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventlistenerTransactionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
